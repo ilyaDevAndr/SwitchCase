eeeba3d71335aed0d3f3898b89ddd93f681b37cd
ilya1 <ilya1@DESKTOP-R4A653G>
2022-10-21 18:22:05 +0300

    Добавьте файлы проекта.

---
+++ SwitchCase/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SwitchCase
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Козырского Ильи 24ис");
+            Console.WriteLine("Введите оценку");
+            String selection = Console.ReadLine(); // строковая переменная 
+            switch (selection)// сравниваем выражение
+            {
+                case "1": // значение выражения
+                    Console.WriteLine("плохо");
+                    break;
+                case "2":// значение выражения
+                    Console.WriteLine("неудовлетворительно");
+                    break;
+                case "3":// значение выражения
+                    Console.WriteLine("Удовлетворительно ");
+                    break;
+                case "4":// значение выражения
+                    Console.WriteLine("Хорощо");
+                    break;
+                case "5":// значение выражения
+                    Console.WriteLine("отлично");
+                    break;
+                default:// совпадений нет
+                    Console.WriteLine("ошибки при вводе ");
+                    break;
+            }
+            Console.ReadKey();// конец программы
+
+        }
+    }
+}
